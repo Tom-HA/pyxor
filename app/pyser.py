ff14e261d0e72bc946e541dad77681e1b2165152
Tom <tom.harpaz98@gmail.com>
2022-11-04 16:50:41 +0200

    implement web api

---
+++ app/pyser.py
@@ -0,0 +1,24 @@
+from flask import Flask, jsonify, request
+from app import parser
+
+app = Flask(__name__)
+
+
+@app.route("/health")
+def health_check():
+    return '', 200
+
+
+@app.route('/api/yaml_extract', methods=['POST'])
+def extract_yaml():
+    if not request.is_json:
+        return jsonify({"data": "Content-type was not detected as application/json"}), 400
+    try:
+        data = request.get_json()
+        r = parser.extract_values_from_yaml(data['text'], data['expr'])
+        resp = jsonify({"data": r})
+        return resp, 200
+    except Exception as e:
+        return jsonify({"data": str(e)}), 400
+    
+
