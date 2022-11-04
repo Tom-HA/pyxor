94418bef89aed56a28191eae3eea73b0d17ede08
Tom <33584517+Tom-HA@users.noreply.github.com>
2022-11-04 16:52:55 +0200

    implement web api (#9)

---
+++ app/parser.py
@@ -0,0 +1,16 @@
+from jsonpath_rw import jsonpath, parse
+import yaml
+
+def extract_values_from_yaml(stream, expression):
+    try:
+        data = yaml.safe_load(stream)
+        jsonpath_expr = parse(expression)
+        values = []
+        for match in jsonpath_expr.find(data):
+            values.append(match.value)
+        print(values)
+        
+        return values
+    except yaml.YAMLError as e:
+        print(e)
+        return e
