7defbf9531e9ac7e976d95c29b063277ccc59df3
Tom <33584517+Tom-HA@users.noreply.github.com>
2022-11-04 22:40:15 +0200

    Add API and CLI tests (#13)
    * add tests for API and CLI
    
    * update requirements.txt

---
+++ tests/test_command.py
@@ -0,0 +1,20 @@
+from click.testing import CliRunner
+from pyxor.command import extract_yaml_value
+
+def test_extract_yaml_value_stdin():
+  runner = CliRunner()
+  result = runner.invoke(extract_yaml_value, ["--expr", "root.child.text", "--file", "-"], input='root:\n    child:\n        text: test')
+  print(result.output)
+  assert result.output == "['test']\n"
+  assert result.exit_code == 0
+
+def test_extract_yaml_value_file():
+  runner = CliRunner()
+  
+  with runner.isolated_filesystem():
+      with open('test.yaml', 'w') as f:
+          f.write('root:\n    child:\n        text: test')
+  
+      result = runner.invoke(extract_yaml_value, ["--expr", "root.child.text", "--file", "test.yaml"])
+      assert result.output == "['test']\n"
+      assert result.exit_code == 0
---
+++ tests/test_main.py
@@ -0,0 +1,115 @@
+from fastapi.testclient import TestClient
+from pyxor.main import app
+
+client = TestClient(app)
+
+
+def test_health():
+    response = client.get("/health")
+    assert response.status_code == 200
+    assert response.json() == {"alive": "true"}
+
+def test_extract_yaml_string():
+
+    response = client.post(
+        "api/yaml_extract",
+        json={
+            "text": '''root:
+    child1:
+        list:
+            - element1
+            - element2
+        listOfdicts:
+            - key1: element1
+              key2: element2
+    child2:
+        child2t: "text"''',
+            "expr": "root.child2.child2t"
+        }
+    )
+    assert response.json() == {"data": ['text']}
+    assert response.status_code == 200
+
+def test_extract_yaml_list_element():
+
+    response = client.post(
+        "api/yaml_extract",
+        json={
+            "text": '''root:
+    child1:
+        list:
+            - element1
+            - element2
+        listOfdicts:
+            - key1: element1
+              key2: element2
+    child2:
+        child2t: "text"''',
+            "expr": "root.child1.list[0]"
+        }
+    )
+    assert response.json() == {"data": ['element1']}
+    assert response.status_code == 200
+    
+def test_extract_yaml_list():
+
+    response = client.post(
+        "api/yaml_extract",
+        json={
+            "text": '''root:
+    child1:
+        list:
+            - element1
+            - element2
+        listOfdicts:
+            - key1: element1
+              key2: element2
+    child2:
+        child2t: "text"''',
+            "expr": "root.child1.list"
+        }
+    )
+    assert response.json() == {"data": [['element1', 'element2']]}
+    assert response.status_code == 200
+
+def test_extract_yaml_key_from_list_of_dicts():
+
+    response = client.post(
+        "api/yaml_extract",
+        json={
+            "text": '''root:
+    child1:
+        list:
+            - element1
+            - element2
+        listOfdicts:
+            - key1: element1
+              key2: element2
+    child2:
+        child2t: "text"''',
+            "expr": "root.child1.listOfdicts[0].key1"
+        }
+    )
+    assert response.json() == {"data": ['element1']}
+    assert response.status_code == 200
+
+def test_extract_yaml_key():
+
+    response = client.post(
+        "api/yaml_extract",
+        json={
+            "text": '''root:
+    child1:
+        list:
+            - element1
+            - element2
+        listOfdicts:
+            - key1: element1
+              key2: element2
+    child2:
+        child2t: "text"''',
+            "expr": "root.child2"
+        }
+    )
+    assert response.json() == {"data": [{"child2t": "text"}]}
+    assert response.status_code == 200
