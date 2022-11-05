090b5869ade6f1a83563356a051ebe28ca3c7571
Tom <33584517+Tom-HA@users.noreply.github.com>
2022-11-05 19:33:51 +0200

    Code enhancements (#21)
    * update return value
    
    * add logs, handle exceptions, change return values, and update tests

--- pyxor/main.py
+++ pyxor/main.py
@@ -1,8 +1,14 @@
+import os
+import logging
 import uvicorn
 from pydantic import BaseModel
-from fastapi import FastAPI, HTTPException
+from jsonpath_rw.lexer import JsonPathLexerError
+from yaml import YAMLError
+from fastapi import FastAPI, Response, status
+from fastapi.encoders import jsonable_encoder
 from internal import parser
 
+logging.basicConfig(level=os.environ.get("LOGLEVEL", "INFO"), format='%(levelname)s:\t\t%(message)s')
 app = FastAPI()
 
 class Extraction(BaseModel):
@@ -15,13 +21,29 @@ def health_check():
 
 
 @app.post("/api/yaml_extract")
-def extract_yaml(extraction: Extraction):
+def extract_yaml(extraction: Extraction, response: Response):
     try:
         r = parser.extract_values_from_yaml(extraction.text, extraction.expr)
-        resp = {"data": r}
-        return resp
+    except YAMLError as e:
+        logging.warning(e)
+        response.status_code = status.HTTP_422_UNPROCESSABLE_ENTITY
+        return {"data": "Failed to parse YAML"}
+    except JsonPathLexerError as e:
+        logging.warning(e)
+        response.status_code = status.HTTP_422_UNPROCESSABLE_ENTITY
+        return {"data": "Failed to parse YAML path expression"}
+    except AttributeError as e:
+        logging.warning(e)
+        response.status_code = status.HTTP_422_UNPROCESSABLE_ENTITY
+        return {"data": "Failed to parse request"}
+        
     except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
+        logging.error(e)
+        response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+        return {"data": "Unhandled exception"}
+        
+    resp = {"data": r}
+    return resp
     
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=5001)
--- pyxor/pyxor.py
+++ pyxor/pyxor.py
@@ -1,5 +1,12 @@
+import sys
 import click
+import os
+import logging
+from jsonpath_rw.lexer import JsonPathLexerError
+from yaml import YAMLError
 from internal import parser
+
+logging.basicConfig(level=os.environ.get("LOGLEVEL", "INFO"), format='%(levelname)s:\t\t%(message)s')
 
 @click.command()
 @click.option('--expr', '-e', type=str, required=True, help='The expression to extract')
@@ -14,8 +21,25 @@ def extract_yaml_value(file: str, expr: str):
             data = file.read()
         except Exception as e:
             print(e)
-    values = parser.extract_values_from_yaml(data, expr)
-
+    try:
+        values = parser.extract_values_from_yaml(data, expr)
+    except YAMLError as e:
+        logging.debug(e)
+        print("Failed to parse YAML")
+        sys.exit(1)
+    except JsonPathLexerError as e:
+        logging.debug(e)
+        print("Failed to parse YAML path expression")
+        sys.exit(1)
+    except AttributeError as e:
+        logging.debug(e)
+        print(e)
+        sys.exit(1)
+    except Exception as e:
+        logging.debug(e)
+        print("Unhandled error")
+        sys.exit(1)
+        
     print(values)
 
 if __name__ == '__main__':
