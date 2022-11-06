4591afd03e98a31359c02a7df32981d76adf64aa
Tom <33584517+Tom-HA@users.noreply.github.com>
2022-11-06 12:02:41 +0200

    Remove redundant exception handling (#31)

--- pyxor/internal/parser.py
+++ pyxor/internal/parser.py
@@ -1,19 +1,14 @@
-from jsonpath_rw import parse, lexer
-from yaml import safe_load, YAMLError
+from jsonpath_rw import parse
+from yaml import safe_load
 
 def extract_values_from_yaml(stream, expression):
-    try:
-        data = safe_load(stream)
-    except YAMLError as e:
-        raise e
-    try:
-        jsonpath_expr = parse(expression)
-    except lexer.JsonPathLexerError as e:
-        raise e
-    
+    data = safe_load(stream)
+    jsonpath_expr = parse(expression)
+   
     values = []
     for match in jsonpath_expr.find(data):
         values.append(match.value)
+
     if len(values) == 1:
         return {"data": values[0]}
     
